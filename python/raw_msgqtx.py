7a61f871669ac2d8a387e6b9a0628fb06502accd
Jonathan Liang <ljxangus@gmail.com>
2017-10-11 21:28:51 +0800

    * raw_message_queue modified

--- python/raw_msgqtx.py
+++ python/raw_msgqtx.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 #
 # Copyright 2005,2006,2011 Free Software Foundation, Inc.
-# 
+#
 # This file is part of GNU Radio
-# 
+#
 # GNU Radio is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # GNU Radio is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with GNU Radio; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 #
 # @author: lzyou@inc.cuhk.edu.hk
@@ -56,7 +56,7 @@ class my_top_block(gr.top_block):
         self.txpath = gr.message_source(gr.sizeof_gr_complex, 3)
         self.amp = gr.multiply_const_cc(options.amp)
         self.connect(self.txpath, self.amp, self.sink)
-        
+
 # /////////////////////////////////////////////////////////////////////////////
 #                                   main
 # /////////////////////////////////////////////////////////////////////////////
@@ -78,7 +78,7 @@ def main():
     parser.add_option("-n", "--num", type="eng_float", default=1,
                       help="set number of packets [default=%default]")
     parser.add_option("-g", "--gap", type="eng_float", default=0.005,
-                      help="set number of packets [default=%default]")
+                      help="set number of samples between each transmission [default=%default]")
     parser.add_option("","--data-file", default=None,
                       help="use complex input file for transmission")
     parser.add_option("","--to-file", default=None,
@@ -86,7 +86,7 @@ def main():
     parser.add_option("-W", "--bandwidth", type="eng_float",
                       default=4e6,
                       help="set symbol bandwidth [default=%default]")
-    parser.add_option("", "--amp", type="eng_float", default=0.1, 
+    parser.add_option("", "--amp", type="eng_float", default=0.1,
                       help="set gain factor for complex baseband floats [default=%default]")
     parser.add_option("", "--time", action="store_true", default=False,
                       help="set timed tx mode")
@@ -99,13 +99,13 @@ def main():
 
     # build the graph
     tb = my_top_block(options)
-    
+
     r = gr.enable_realtime_scheduling()
     if r != gr.RT_OK:
         print "Warning: failed to enable realtime scheduling"
 
     tb.start()                       # start flow graph
-    
+
     ###########################################################################
     if options.data_file is None:
         sys.stderr.write("You must specify data file\n")
@@ -139,7 +139,7 @@ def main():
     print "End of Tx | cnt = ", cnt
     time.sleep(1)
     ###########################################################################
-    
+
     tb.wait()                       # wait for it to finish
 
 if __name__ == '__main__':
